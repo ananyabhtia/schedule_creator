8cd8c1cacb17af836621423f3d5d28d22e2cc667
ananyabhtia <ananyabhtia@gmail.com>
2024-10-16 21:44:02 -0500

    check :)

--- frontend/app.js
+++ frontend/app.js
@@ -31,5 +31,5 @@ app.get('/times-data', (req, res) => {
   });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port} :)`)
   })
